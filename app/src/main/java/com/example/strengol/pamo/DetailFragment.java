e64941331241accf85e9274230586b166ed7b8af
DESKTOP-QN6JENK\Strengol <Strengol1@gmail.com>
2017-01-07 00:29:05 +0100

    new project

---
+++ app/src/main/java/com/example/strengol/pamo/DetailFragment.java
@@ -0,0 +1,28 @@
+package com.example.strengol.pamo;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DetailFragment extends android.app.Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater
+                .inflate(R.layout.fragment_detail, container, false);
+        return view;
+    }
+
+    public void setText(String txt) {
+        TextView view = (TextView) getView().findViewById(R.id.detailsText);
+        view.setText(txt);
+    }
+}
